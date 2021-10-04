02ee486069c20f5b201b1c9244d67a6de9fbfdf4
Abhijit2404 <Abhijit.Shinde@blueconchtech.com>
2021-10-04 13:58:37 +0530

    delete-user

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using LoginMVC.Models;
 using LoginMVC.Services;
@@ -25,14 +27,45 @@ namespace LoginMVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(UserCreate user)
+        public IActionResult Create(UserCreate user)
         {
-            bool success = await _userService.SaveUser(user);
-            if(success)
+            // bool success = await _userService.SaveUser(user);
+            // if(success)
+            // {
+            //     return RedirectToAction("Index","Home");
+            // }
+            // return RedirectToAction("Index","Home");
+                using(var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://localhost:5001");
+                    var postjob = client.PostAsJsonAsync<UserCreate>("users",user);
+                    postjob.Wait();
+
+                    var result = postjob.Result;
+                    if(result.IsSuccessStatusCode){
+                        return RedirectToAction("List");
+                    }
+                }
+            ModelState.AddModelError(string.Empty,"Server Error");
+            return View(user);
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            using (var client = new HttpClient())
             {
-                return RedirectToAction("List");
+                client.BaseAddress = new Uri("https://localhost:5001");
+
+                var deleteTask = client.DeleteAsync("users/Id?Id=" + Id.ToString());
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("List");
+                }
             }
-            return RedirectToAction("Index","Home");
+        return RedirectToAction("Index","Home");
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,9 @@ namespace LoginMVC.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public int UserHospitalId { get; set; }
+        // public int UserHospitalId { get; set; }
     }
 }
