7bef168269668d6c21a598fc69ccff51d3d4675b
Abhijit2404 <Abhijit.Shinde@blueconchtech.com>
2021-10-06 13:17:43 +0530

    .

--- Models/UserCreate.cs
+++ Models/UserCreate.cs
@@ -5,7 +5,6 @@ namespace LoginMVC.Models
 {
     public class UserCreate
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
